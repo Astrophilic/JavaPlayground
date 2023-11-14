ef915db3297f2b771e26aaa3ef85ef4700c6bfe9
astrophilic <pankajhz99@gmail.com>
2023-11-14 16:21:20 +0530

    squashing f8 and f9 f6 into f1 as as well
    f4 and f5 also f2 and f3 squashed

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,14 @@ class Solution {
 
 
         TextContext.runExample();
+        System.out.println("f1");
+        System.out.println("f2");
+        System.out.println("f3");
+        System.out.println("f4");
+        System.out.println("f5");
 
-
+        System.out.println("f6");
+        System.out.println("f7");
+        System.out.println("f8");
         }
     }
